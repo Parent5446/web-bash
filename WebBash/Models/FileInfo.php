f5e9ed74270fe659e451456683b4ebbd27de8ee4
Kris <krisjj123@gmail.com>
2013-06-26 20:14:51 -0400

    Added get file contents function

--- WebBash/Models/FileInfo.php
+++ WebBash/Models/FileInfo.php
@@ -259,4 +259,25 @@ class FileInfo implements Model
 		$this->load();
 		return $this->exists;
 	}
+
+	public function getContents( $offset, $length ) {
+		isAllowed( $this->deps->currentUser, ( ACTION_READ + ACTION_EXECUTE ) ) {
+			$fp = fopen( $this->path, 'r' );
+
+			$fileLength = fseek( $fp, 0, SEEK_END );
+
+			if ( $fileLength < ($offset + $length) ) {
+				throw new HttpException( 403, 'Accessing memory outside of file' );
+			}
+
+			rewind( $fp );
+			fseek( $fp, $offset );
+
+			$contents = fread( $fp, $length );
+			fclose( $fp );
+
+			return $contents;
+		}
+		throw new HttpException( 403, 'Insufficient permissions to access file contents' );
+	}
 }
