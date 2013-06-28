d18782e0018c4faadcd6a67e7de5abe672c18c49
unknown <krisjj123@gmail.com>
2013-06-27 20:29:12 -0400

    i don't know what the fuck i'm doing

--- static/jquery.printfile.js
+++ static/jquery.printfile.js
@@ -7,8 +7,27 @@
 	 * @param responseJSON (dunno) ***********************************
 	 * @param opts {Array.<string>} opts
 	 */
-
 	 $.printfile = function( fd, responseJSON, opts ) {
+	 	var name = responseJSON;
+	 	var output = name;
+	 	bool printDot = false;
+
+	 	foreach ( var option in opts ) {
+	 		if ( opts.hasOwnProperty( option) ) {
+	 			switch ( option ) {
+	 				case 'a':
+	 					printDot = true;
+	 					break;
+	 				case 'l':
+	 					// add stuff here
+	 					break;
+
+	 			}
+	 		}
+	 	}
+	 	
+
+	 	if( name[0] !== '.' || printDot )
 	 	fd.write( responseJSON + "\n" );
 	 };
 } )( jQuery );
